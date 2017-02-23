ddcaad5595d530578e7ff40fc768fa03e1d38528
Gregory <grihabor@mail.ru>
2017-02-24 01:01:11 +0400

    Create hello_world.py

---
+++ elementary/hello_world.py
@@ -0,0 +1,26 @@
+# To print anything to the screen use print() function
+# Examples:
+
+print('example')
+print("second example")
+print("third", 'one')
+
+### At this point you can already complete the task!
+
+# Here are some details on what's going on
+
+# When you type some characters inside a pair of parentheses
+# you create an object of type 'str' that holds the string.
+
+# After that you can link the object with a name:
+
+a = 'some string'
+
+# In this example we associate name 'a' with a string
+# with a string 'some string'. Now we can pass it to print:
+
+print(a)
+
+# Or we could do that without an intermedeate step:
+
+print('some string')
